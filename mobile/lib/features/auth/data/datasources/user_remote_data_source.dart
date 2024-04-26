ef3a5addc61d8abcc9ff4eaf0d7ab2ba4527f9f8
afif-ayoubi <afifayoubi22@gmail.com>
2024-04-26 21:39:26 +0300

    add method to remote data source

--- mobile/lib/features/auth/data/datasources/user_remote_data_source.dart
+++ mobile/lib/features/auth/data/datasources/user_remote_data_source.dart
@@ -1,3 +1,43 @@
+import 'package:dartz/dartz.dart';
+import 'package:mobile/features/auth/data/models/user_model.dart';
+
 abstract class UserRemoteDataSource {
+  Future<Unit> createUser(UserModel user);
+  Future<Unit> login(UserModel user);
+  Future<Unit> logout();
+  Future<Unit> updateUser(UserModel user);
+  Future<UserModel> getUser();
+}
+
+class UserRemoteDataSourceImpl implements UserRemoteDataSource {
+  @override
+  Future<Unit> createUser(UserModel user) {
+    // TODO: implement createUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<UserModel> getUser() {
+    // TODO: implement getUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Unit> login(UserModel user) {
+    // TODO: implement login
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Unit> logout() {
+    // TODO: implement logout
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Unit> updateUser(UserModel user) {
+    // TODO: implement updateUser
+    throw UnimplementedError();
+  }
 
 }
