da55d93287303537c5ac42e280d27c7ca5cabb7a
afif-ayoubi <afifayoubi22@gmail.com>
2024-04-27 19:02:09 +0300

    adjust some constant

--- mobile/lib/core/constants/font_manager.dart
+++ mobile/lib/core/constants/font_manager.dart
@@ -1,3 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 class FontConstants{
   static const String fontFamily = 'Roboto';
+}
+class FontWeightManager {
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
+}
+
+class FontSize {
+  static double s4 = 4.0.sp;
+  static double s10 = 10.0.sp;
+  static double s12 = 12.0.sp;
+  static double s14 = 14.0.sp;
+  static double s16 = 16.0.sp;
+  static double s17 = 17.0.sp;
+  static double s18 = 18.0.sp;
+  static double s20 = 20.0.sp;
+  static double s22 = 22.0.sp;
+  static double s24 = 24.0.sp;
+  static double s26 = 26.0.sp;
+  static double s28 = 28.0.sp;
+  static double s30 = 30.0.sp;
+  static double s32 = 32.0.sp;
+  static double s40 = 40.0.sp;
 }
