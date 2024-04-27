9a3c477c62540b32286eb050259f4c8cd11a6767
afif-ayoubi <afifayoubi22@gmail.com>
2024-04-27 14:44:43 +0300

    add the events and the state

---
+++ mobile/lib/features/auth/presentation/bloc/users/users_bloc.dart
@@ -0,0 +1,53 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:mobile/core/constants/failure.dart';
+import 'package:mobile/core/error/failures.dart';
+import 'package:mobile/features/auth/domain/usecases/create_user.dart';
+import 'package:mobile/features/auth/domain/usecases/get_user.dart';
+import 'package:mobile/features/auth/domain/usecases/login_user.dart';
+import 'package:mobile/features/auth/domain/usecases/logout_user.dart';
+import 'package:mobile/features/auth/domain/usecases/update_user.dart';
+
+part 'users_event.dart';
+part 'users_state.dart';
+
+class UsersBloc extends Bloc<UsersEvent, UsersState> {
+  final CreateUserUseCase createUserUseCase;
+  final LoginUserUseCase loginUserUseCase;
+  final LogoutUserUseCase logoutUserUseCase;
+  final UpdateUserUseCase updateUserUseCase;
+  final GetUserUseCase getUserUseCase;
+  UsersBloc(
+      {required this.createUserUseCase,
+      required this.getUserUseCase,
+      required this.loginUserUseCase,
+      required this.logoutUserUseCase,
+      required this.updateUserUseCase})
+      : super(UsersInitial()) {
+    on<UsersEvent>((event, emit) async {
+      if (event is CreateUserEvent) {
+        emit(LoadingUsersState());
+      } else if (event is LoginUserEvent) {
+        emit(LoadingUsersState());
+      } else if (event is LogoutUserEvent) {
+        emit(LoadingUsersState());
+      } else if (event is UpdateUserEvent) {
+        emit(LoadingUsersState());
+      } else if (event is GetUserEvent) {
+        emit(LoadingUsersState());
+      }
+    });
+  }
+   String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure _:
+        return serverFailureMessage;
+      case EmptyCacheFailure _:
+        return emptyCacheFailureMessage;
+      case OfflineFailure _:
+        return offlineFailureMessage;
+      default:
+        return "Unexpected  Error, Please try again later";
+    }
+  }
+}
