2b579d1070e45ad0dd50920cf4631435c0338e50
afif-ayoubi <afifayoubi22@gmail.com>
2024-04-27 19:01:37 +0300

    dependency injection file

---
+++ mobile/lib/injection_container.dart
@@ -0,0 +1,62 @@
+import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:mobile/core/network/network_info.dart';
+import 'package:mobile/features/auth/data/datasources/user_remote_data_source.dart';
+import 'package:mobile/features/auth/domain/repositories/user_repository.dart';
+import 'package:mobile/features/auth/domain/usecases/create_user.dart';
+import 'package:mobile/features/auth/domain/usecases/get_user.dart';
+import 'package:mobile/features/auth/domain/usecases/login_user.dart';
+import 'package:mobile/features/auth/domain/usecases/logout_user.dart';
+import 'package:mobile/features/auth/domain/usecases/update_user.dart';
+import 'package:mobile/features/auth/presentation/bloc/users/users_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'features/auth/data/repositories/user_repository_impl.dart';
+import 'package:http/http.dart' as http;
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  //! Features - Users
+
+  // Bloc
+
+  sl.registerFactory(() => UsersBloc(
+      createUserUseCase: sl(),
+      getUserUseCase: sl(),
+      loginUserUseCase: sl(),
+      logoutUserUseCase: sl(),
+      updateUserUseCase: sl()));
+
+  // UseCases
+
+  sl.registerLazySingleton(() => CreateUserUseCase(sl()));
+  sl.registerLazySingleton(() => LoginUserUseCase(sl()));
+  sl.registerLazySingleton(() => LogoutUserUseCase(sl()));
+  sl.registerLazySingleton(() => UpdateUserUseCase(sl()));
+  sl.registerLazySingleton(() => GetUserUseCase(sl()));
+
+  // Repository
+
+  sl.registerLazySingleton<UsersRepository>(() => UsersRepositoryImpl(
+        remoteDataSource: sl(),
+        localDataSource: sl(),
+        networkInfo: sl(),
+      ));
+
+  //DataSources
+
+  sl.registerLazySingleton<UserRemoteDataSource>(
+      () => UserRemoteDataSourceImpl(client: sl()));
+
+  //! core
+
+  sl.registerLazySingleton<NetworkInfo>(() => NetwerkInfoImpl(sl()));
+
+  //! External
+  
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerFactory(() => http.Client());
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+}
