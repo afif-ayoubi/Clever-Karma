d482b3d39f38e81e99a538b274c1751cc2718d15
afif-ayoubi <afifayoubi22@gmail.com>
2024-04-28 16:09:06 +0300

    initialize the live streaming schema

---
+++ backend/src/schemas/live_streaming.schema.ts
@@ -0,0 +1,6 @@
+import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
+
+export class LiveStreaming {
+    
+ }
+export const LiveStreamingSchema = SchemaFactory.createForClass(LiveStreaming);
