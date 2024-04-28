185105bb4d52794bb3ad69fa21c4e44975695a46
afif-ayoubi <afifayoubi22@gmail.com>
2024-04-28 19:13:12 +0300

    create onboarding btn

--- mobile/lib/features/auth/presentation/widgets/onboarding_page/onboarding_btn.dart
+++ mobile/lib/features/auth/presentation/widgets/onboarding_page/onboarding_btn.dart
@@ -1,15 +1,23 @@
-import 'package:flutter/material.dart';
+  import 'package:flutter/material.dart';
+  import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mobile/core/theme/hex_color.dart';
 
-class OnBoardingBtn extends StatelessWidget {
-  final void Function() onPressed;
-  const OnBoardingBtn({super.key, required this.onPressed});
+  class OnBoardingBtn extends StatelessWidget {
+    final void Function() onPressed;
+    const OnBoardingBtn({super.key, required this.onPressed});
 
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton.icon(
-      onPressed: onPressed,
-      icon: const Icon(Icons.arrow_forward_outlined),
-      label: const Text(''),
-    );
+    @override
+    Widget build(BuildContext context) {
+      return SizedBox(
+        height: 80.h,
+        width: 80.w,
+        child: ElevatedButton(
+          onPressed: onPressed,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: HexColor.whiteColor,
+          ),
+          child: const Icon(Icons.arrow_forward_ios_sharp),
+        ),
+      );
+    }
   }
-}
