e7e520164659f127d1b2126b8fa86d44d200ba56
afif-ayoubi <afifayoubi22@gmail.com>
2024-04-28 20:41:30 +0300

    create onboarding screen

--- mobile/lib/features/auth/presentation/pages/onboarding_page.dart
+++ mobile/lib/features/auth/presentation/pages/onboarding_page.dart
@@ -1,16 +1,49 @@
-
 import 'package:flutter/material.dart';
-import 'package:mobile/features/auth/presentation/widgets/onboarding_page/onboarding_deatil_widget.dart';
+import 'package:mobile/features/auth/presentation/widgets/onboarding_page/onboarding_detail_widget.dart';
 
 class OnBoardingPage extends StatelessWidget {
   const OnBoardingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: _buildBody(context));
+    return Scaffold(
+      body: _buildBody(context),
+    );
   }
 }
 
 Widget _buildBody(BuildContext context) {
-  return OnBoardingDetailWidget();
+  List<Map<String, dynamic>> onBoardingData = [
+    {
+      'title': "Find Your Passion",
+      'body': "What issues matter most to you",
+      'isFirst': true,
+      'imgPath': '',
+    },
+    {
+      'title': "Making a Difference\n Together",
+      'body': "Explore Causes You Care\n About",
+      'isFirst': false,
+      'imgPath': '',
+    },
+    {
+      'title': "Your Impact Starts\n Here",
+      'body': "Choose Causes That Align With\n Your Value",
+      'isFirst': false,
+      'imgPath': '',
+    },
+  ];
+  return SizedBox(
+     child:  ListView.builder(
+        itemCount: onBoardingData.length,
+        itemBuilder: (context, index) {
+          return OnBoardingDetailWidget(
+            title: onBoardingData[index]['title'],
+            body: onBoardingData[index]['body'],
+            isFirst: onBoardingData[index]['isFirst'],
+            imgPath: onBoardingData[index]['imgPath'],
+          );
+        },
+      )
+  );
 }
---
+++ mobile/lib/features/auth/presentation/widgets/onboarding_page/onboarding_detail_widget.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mobile/core/constants/assets_manager.dart';
+import 'package:mobile/features/auth/presentation/widgets/onboarding_page/onboarding_btn.dart';
+
+class OnBoardingDetailWidget extends StatelessWidget {
+  final String title;
+  final String body;
+  final bool isFirst;
+  final String imgPath;
+  const OnBoardingDetailWidget(
+      {super.key,
+      required this.title,
+      required this.body,
+      this.isFirst = false,
+      required this.imgPath});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).textTheme;
+
+    return ColorFiltered(
+      colorFilter:
+          ColorFilter.mode(Colors.black.withOpacity(0.6), BlendMode.difference),
+      child: Container(
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(AppImages.onBoardingImg1),
+                fit: BoxFit.cover)),
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Animate(
+                    
+                    child: Text(
+                      "Find Your Passion",
+                      style: theme.displayLarge,
+                    ).animate().fade(duration: 1.seconds).slideY(curve: Curves.easeInOut,),
+                  ),
+                  Padding(
+                    padding: isFirst
+                        ? EdgeInsets.only(top: 50.h)
+                        : EdgeInsets.only(top: 25.h),
+                    child: Text(
+                      "What issues matter most to you",
+                      style: theme.bodyLarge,
+                      textAlign: TextAlign.center,
+                    )..animate().fade(duration: 2.seconds).slideY(curve: Curves.easeInOut,),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 150.h,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: OnBoardingBtn(onPressed: () {}),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
