19d7bebcae6bad6c3d27e7ef43ddf219f0573c23
afif-ayoubi <afifayoubi22@gmail.com>
2024-04-29 13:38:04 +0300

    adding 2 colors

--- mobile/lib/core/theme/hex_color.dart
+++ mobile/lib/core/theme/hex_color.dart
@@ -3,5 +3,7 @@ import 'package:flutter/material.dart';
 class HexColor{
 static const Color primaryColor = Color(0xFF058789); 
 static const Color secondaryColor = Color(0xFFFF914D);
+static const Color lightColor = Color(0xFFE7E7E7);
+static const Color textInputColor = Color(0xFFB5B4B4);
 static const Color whiteColor = Color(0xFFFFFFFF);
 }
