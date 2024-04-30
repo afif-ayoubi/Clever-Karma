5c3226a84921dbc3569cde285eda8608a039dfef
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-01 00:36:51 +0300

    added color

--- mobile/lib/core/theme/hex_color.dart
+++ mobile/lib/core/theme/hex_color.dart
@@ -6,4 +6,5 @@ static const Color secondaryColor = Color(0xFFFF914D);
 static const Color lightColor = Color(0xFFE7E7E7);
 static const Color textInputColor = Color(0xFFB5B4B4);
 static const Color whiteColor = Color(0xFFFFFFFF);
+static const Color borderColor = Color(0xFFD2D2D2);
 }
