0b329aa7235fb678e76bf46fbe9af2f493f4ca0a
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-02 15:40:14 +0300

    added  create api

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { User } from "src/schemas/user.schema";
+import { CreateUserDto } from "./dto/create_user.dto";
 
 @Injectable()
-export class UsersService{
-    constructor(@InjectModel(User.name) private userModel:Model<User>){
-        // createUser(createUserDto)
-         
+export class UsersService {
+    constructor(@InjectModel(User.name) private userModel: Model<User>) { }
+    createUser(createUserDto: CreateUserDto) {
+        const user=new this.userModel(createUserDto);
+        return user.save();
     }
 }
