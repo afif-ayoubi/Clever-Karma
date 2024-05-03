08b2e302a5c732ea4dfef7996e589cdee9e82f09
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-03 14:20:12 +0300

    made it abstract

--- mobile/lib/core/theme/hex_color.dart
+++ mobile/lib/core/theme/hex_color.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class HexColor{
+abstract class HexColor{
 static const Color primaryColor = Color(0xFF058789);
 static const Color secondaryColor = Color(0xFFFF914D);
 static const Color lightColor = Color(0xFFE7E7E7);
