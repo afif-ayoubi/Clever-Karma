04fc4a3cb7f70da24ef34d0d3daecd606f8295eb
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-03 20:07:22 +0300

    created the  home page

---
+++ mobile/lib/features/opportunity/presentation/pages/home_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
+import 'package:mobile/core/constants/font_manager.dart';
+import 'package:mobile/core/extensions/text_theme.dart';
+import 'package:mobile/features/opportunity/presentation/widgets/home_page/opportunity_view.dart';
+
+import '../widgets/home_page/lighted_backround.dart';
+import '../widgets/home_page/page_indicators.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = PageController(viewportFraction: 0.8);
+  final ValueNotifier<double> pageNotifier = ValueNotifier(0);
+  final ValueNotifier<int> roomSelectorNotifier = ValueNotifier(-1);
+
+  @override
+  void initState() {
+    controller.addListener(pageListener);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller
+      ..removeListener(pageListener)
+      ..dispose();
+    super.dispose();
+  }
+
+  void pageListener() {
+    pageNotifier.value = controller.page ?? 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LightedBackground(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SafeArea(
+          child: Column(
+            children: [
+              Gap(100.h),
+              Text("Select Your Cause", style: context.displayLarge!.copyWith(fontWeight: FontWeightManager.regular)),
+
+              Expanded(
+                child: Stack(
+                  fit: StackFit.expand,
+                  clipBehavior: Clip.none,
+                  children: [
+                    OpportunityView(
+                      pageNotifier: pageNotifier,
+                      roomSelectorNotifier: roomSelectorNotifier,
+                      controller: controller,
+                    ),
+                    Positioned.fill(
+                      top: null,
+                      child: Column(
+                        children: [
+                          PageIndicators(
+                            roomSelectorNotifier: roomSelectorNotifier,
+                            pageNotifier: pageNotifier,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
