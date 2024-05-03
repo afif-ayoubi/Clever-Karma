e427a47e269243925633b1cb77122c395a2e3a50
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-03 20:06:22 +0300

    created the parallex image card

---
+++ mobile/lib/features/opportunity/presentation/widgets/home_page/parallax_image_card.dart
@@ -0,0 +1,52 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mobile/core/theme/hex_color.dart';
+
+
+class ParallaxImageCard extends StatelessWidget {
+  const ParallaxImageCard(
+      {super.key, required this.imageUrl, this.parallaxValue = 0});
+
+  final String imageUrl;
+  final double parallaxValue;
+
+  BoxDecoration get _perallaxUrlImageDecoration => BoxDecoration(
+    borderRadius: BorderRadius.all(const Radius.circular(12).w),
+    color: HexColor.lightColor,
+    boxShadow: const [
+      BoxShadow(
+        color: Colors.black26,
+        blurRadius: 12,
+        offset: Offset(-7, 7),
+      ),
+    ],
+    image: DecorationImage(
+      image: AssetImage(imageUrl),
+      fit: BoxFit.cover,
+      colorFilter:
+      const ColorFilter.mode(Colors.black26, BlendMode.colorBurn),
+      alignment: Alignment(lerpDouble(.5, -.5, parallaxValue)!, 0),
+    ),
+  );
+
+  BoxDecoration get _vignetteDecoration => BoxDecoration(
+    borderRadius: BorderRadius.all(Radius.circular(12).w),
+    gradient: const RadialGradient(
+      radius: 2,
+      colors: [Colors.transparent, Colors.black],
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        DecoratedBox(decoration: _perallaxUrlImageDecoration),
+        DecoratedBox(decoration: _vignetteDecoration),
+      ],
+    );
+  }
+}
