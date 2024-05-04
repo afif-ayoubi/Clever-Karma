22700eb44029528dc89e6aa45555d65416456266
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-04 16:44:16 +0300

    created app bar for organizations page

---
+++ mobile/lib/features/opportunity/presentation/widgets/organization_page/custom_app_bar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mobile/core/constants/icons_manager.dart';
+import 'package:mobile/core/theme/hex_color.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        context.pop();
+      },
+      child: Container(
+        height: 40.h,
+        width: 40.w,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: HexColor.whiteColor,
+        ),
+        child: const Icon(
+          IconManager.arrowBack,
+        ),
+      ),
+    );
+  }
+}
