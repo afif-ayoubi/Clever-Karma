e10eea1536b3989dff3138daaaded935ae8acd54
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-05 18:38:40 +0300

    adjusted methods

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -2,14 +2,21 @@ import { Body, Controller, Get, HttpException, Param, Patch, Post } from "@nestj
 import { UsersService } from "./users.service";
 import { CreateUserDto } from "./dto/create_user.dto";
 import mongoose from "mongoose";
+import { UserResponseType } from "./types/user_response_type";
 
 @Controller('users')
 export class UsersController {
     constructor(private userService: UsersService) { }
-    @Post()
-    createUser(@Body() createUserDto: CreateUserDto) {
+    @Post('/create')
+    async createUser(@Body() createUserDto: CreateUserDto):Promise<UserResponseType> {
+        const user=await this.userService.createUser(createUserDto);
         console.log(createUserDto);
-        return this.userService.createUser(createUserDto);
+        return this.userService.buildUserResponse(user);
+    }
+    @Post('/login')
+    async login(@Body() loginDto: loginDto):Promise<UserResponseType> {
+        const user=await this.userService.loginUser(loginDto);
+        return this.userService.buildUserResponse(user);
     }
     @Get()
     getUsers() {
@@ -21,10 +28,11 @@ export class UsersController {
         if (!isValid) throw new HttpException("Invalid id", 404);
         const findUser = await this.userService.getUserById(id);
         if (!findUser) throw new HttpException("User not found", 404);
+        return findUser;
     }
-    @Patch(':id')
-    updateUser(@Param('id') id: string, @Body() createUserDto: CreateUserDto) {
-        return this.userService.updateUser(id, createUserDto);
-    }
+    // @Patch(':id')
+    // updateUser(@Param('id') id: string, @Body() createUserDto: CreateUserDto) {
+    //     return this.userService.updateUser(id, createUserDto);
+    // }
 
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,21 +1,32 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { User } from "src/schemas/user.schema";
 import { CreateUserDto } from "./dto/create_user.dto";
+import { UserResponseType } from "./types/user_response_type";
 
 @Injectable()
 export class UsersService {
     constructor(@InjectModel(User.name) private userModel: Model<User>) { }
-    createUser(createUserDto: CreateUserDto) {
-        const user=new this.userModel(createUserDto);
-        return user.save();
+    async createUser(createUserDto: CreateUserDto): Promise<User> {
+        const user = await this.userModel.findOne({ email: createUserDto.email });
+        if (user) throw new HttpException("Email is already taken", HttpStatus.UNPROCESSABLE_ENTITY);
+        const createdUser = new this.userModel(createUserDto);
+        return createdUser.save();
     }
-    getUsers(){
+    getUsers() {
         return this.userModel.find();
     }
-    getUserById(id:String){
+    getUserById(id: String) {
         return this.userModel.findById(id);
     }
-    
+    buildUserResponse(user: User): UserResponseType {
+        return {
+            firstName: user.firstName,
+            lastName: user.lastName, 
+            email: user.email,
+            role: user.role, 
+        };
+    }
+
 }
