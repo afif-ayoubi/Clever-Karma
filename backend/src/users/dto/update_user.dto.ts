b2ac47937aaac62f81e5bbb0bd73fd8ddb43f794
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-06 10:16:56 +0300

    created update dto

--- backend/src/users/dto/update_user.dto.ts
+++ backend/src/users/dto/update_user.dto.ts
@@ -1,3 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
 export class updateUserDto{
-    
+    @IsNotEmpty()
+    @IsString()
+    readonly firstName: string;
+
+    @IsNotEmpty()
+    @IsString()
+    readonly lastName: string;
+
+    @IsNotEmpty()
+    @IsEmail({ require_tld: true }, { message: "Invalid email format" })
+    readonly email: string;
+
 }
