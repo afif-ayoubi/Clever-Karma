55efe4fec2f7a9cfb8a4c50f8d91222c689e0853
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-06 19:34:49 +0300

    adjusted to remove teh splash screen

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -3,13 +3,16 @@ import 'package:flutter/material.dart';
 import 'features/opportunity/core/services/notifications.dart';
 import 'injection_container.dart' as di;
 import 'main_application.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await FirebaseApi.instance.initNotifications();
+  await NotificationService.instance.initNotifications();
+  FlutterNativeSplash.remove();
+
   await di.init();
   runApp(const MyApp());
 }
