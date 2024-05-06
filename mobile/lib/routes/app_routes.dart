13449a6539fb530f3e4d569ae2fe1b2e24efa6b1
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-06 16:50:54 +0300

    added global key

--- mobile/lib/routes/app_routes.dart
+++ mobile/lib/routes/app_routes.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mobile/features/auth/presentation/pages/auth_page.dart';
 import 'package:mobile/features/auth/presentation/pages/forgot_password_page.dart';
@@ -11,34 +12,39 @@ import 'package:mobile/routes/class_routes.dart';
 
 import '../features/opportunity/presentation/pages/entry_page.dart';
 
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
 class AppRoutes {
-  static final GoRouter goRouter =
-      GoRouter(initialLocation: Routes.mapPage, routes: [
-    GoRoute(
-        path: Routes.initRoute,
-        builder: ((context, state) => const OnBoardingPage())),
-    GoRoute(
-        path: Routes.forgotPasswordRoute,
-        builder: ((context, state) => const ForgotPassword())),
-    GoRoute(
-        path: Routes.otpVerificationRoute,
-        builder: ((context, state) => const OtpVerificationPage())),
-    GoRoute(
-        path: Routes.resetPasswordRoute,
-        builder: ((context, state) => const ResetPasswordPage())),
-    GoRoute(
-        path: Routes.profileRoute,
-        builder: ((context, state) => const ProfilePage())),
-    GoRoute(
-        path: Routes.authRoute,
-        builder: ((context, state) => const AuthPage())),
-    GoRoute(
-        path: Routes.entryPage,
-        builder: ((context, state) => const EntryPage())),
-    GoRoute(
-        path: Routes.organizationsPage,
-        builder: ((context, state) => const OrganizationsPage())),
-    GoRoute(
-        path: Routes.mapPage, builder: ((context, state) => const MapPage())),
-  ]);
+  static final GoRouter goRouter = GoRouter(
+      initialLocation: Routes.authRoute,
+      navigatorKey: navigatorKey,
+      routes: [
+        GoRoute(
+            path: Routes.initRoute,
+            builder: ((context, state) => const OnBoardingPage())),
+        GoRoute(
+            path: Routes.forgotPasswordRoute,
+            builder: ((context, state) => const ForgotPassword())),
+        GoRoute(
+            path: Routes.otpVerificationRoute,
+            builder: ((context, state) => const OtpVerificationPage())),
+        GoRoute(
+            path: Routes.resetPasswordRoute,
+            builder: ((context, state) => const ResetPasswordPage())),
+        GoRoute(
+            path: Routes.profileRoute,
+            builder: ((context, state) => const ProfilePage())),
+        GoRoute(
+            path: Routes.authRoute,
+            builder: ((context, state) => const AuthPage())),
+        GoRoute(
+            path: Routes.entryPage,
+            builder: ((context, state) => const EntryPage())),
+        GoRoute(
+            path: Routes.organizationsPage,
+            builder: ((context, state) => const OrganizationsPage())),
+        GoRoute(
+            path: Routes.mapPage,
+            builder: ((context, state) => const MapPage())),
+      ]);
 }
