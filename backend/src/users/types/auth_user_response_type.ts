bb8637d06595f2d1069c3417e0dd9522c2d30639
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-07 20:19:29 +0300

    changed the type name

--- backend/src/users/types/auth_user_response_type.ts
+++ backend/src/users/types/auth_user_response_type.ts
@@ -1,4 +1,4 @@
 import {  Types } from 'mongoose';
 import { User } from "src/schemas/user.schema";
 
-export type UserResponseType = Omit<User, 'password'> & { token: string } & { id: Types.ObjectId };
+export type UserAuthResponseType = Omit<User, 'password'> & { token: string } & { id: Types.ObjectId };
