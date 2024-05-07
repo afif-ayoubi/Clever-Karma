e454d26519ba85c6f865e176a3e389705736600d
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-08 01:13:42 +0300

    adjusted the type of dateOfBorth

--- backend/src/users/dto/update_user.dto.ts
+++ backend/src/users/dto/update_user.dto.ts
@@ -1,30 +1,34 @@
 import { Type } from "class-transformer";
-import { IsDate, IsEmail, IsIn, IsString, ValidateNested } from "class-validator";
+import { IsDate, IsDateString, IsEmail, IsIn, IsOptional, IsString, ValidateNested } from "class-validator";
 import { USER_GENDERS } from "src/utils/user_genders_enum";
 
 export class userInfoDto {
+    @IsOptional()
     @IsString()
     readonly phone?: number;
-
+    @IsOptional()
     @IsIn(Object.values(USER_GENDERS), { message: "Invalid Gender" })
     @IsString()
     readonly gender?: String;
-    @IsDate()
+    @IsOptional()
+    @IsDateString()
     readonly dateOfBirth?: Date;
+    @IsOptional()
     @IsString()
     readonly image?: String;
 }
 
 export class updateUserDto {
+    @IsOptional()
     @IsString()
     readonly firstName: string;
-
+    @IsOptional()
     @IsString()
     readonly lastName: string;
-
+    @IsOptional()
     @IsEmail({ require_tld: true }, { message: "Invalid email format" })
     readonly email: string;
-
+    @IsOptional()
     @ValidateNested({ each: true })
     @Type(() => userInfoDto)
     readonly userInfo: userInfoDto;
