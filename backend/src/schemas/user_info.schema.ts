66d02dddb26a178d79c17a4b20637f9ddbd23911
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-08 01:05:51 +0300

    added validation

--- backend/src/schemas/user_info.schema.ts
+++ backend/src/schemas/user_info.schema.ts
@@ -1,9 +1,10 @@
 import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { USER_GENDERS } from 'src/utils/user_genders_enum';
 
 export class UserInfo {
     @Prop()
     phone?: number;
-    @Prop()
+    @Prop({ enum: [USER_GENDERS.FEMALE, USER_GENDERS.MALE,USER_GENDERS.OTHER], })
     gender?: String;
     @Prop()
     dateOfBirth?: Date;
