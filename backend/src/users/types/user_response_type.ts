c3f0afd13acac4a24c6cceeba514a9038acf4d58
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-07 20:18:10 +0300

    created a new response

--- backend/src/users/types/user_response_type.ts
+++ backend/src/users/types/user_response_type.ts
@@ -1,4 +1,4 @@
-import { ObjectId, Types } from 'mongoose';
+import {  Types } from 'mongoose';
 import { User } from "src/schemas/user.schema";
 
-export type UserResponseType = Omit<User, 'password'> & { token: string } & { id: Types.ObjectId };
+export type UserResponseType = Omit<User, 'password'> & { id: Types.ObjectId };
