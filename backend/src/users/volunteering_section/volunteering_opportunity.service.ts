12a9f5b451e2a775526be0bf8823e2df217e7f1d
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-09 13:56:13 +0300

    created the delete method

--- backend/src/users/volunteering_section/volunteering_opportunity.service.ts
+++ backend/src/users/volunteering_section/volunteering_opportunity.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
-import {  VolunteeringSection } from "src/schemas/volunteering_opportunity.schema";
-import {  CreatedVolunteeringSectionDto } from "./dto/created_volunteering_opportunity.dto";
+import { VolunteeringSection } from "src/schemas/volunteering_opportunity.schema";
+import { CreatedVolunteeringSectionDto } from "./dto/created_volunteering_opportunity.dto";
 
 @Injectable()
 export class VolunteeringSectionService {
@@ -13,5 +13,11 @@ export class VolunteeringSectionService {
         const createdVolunteeringSection = new this.volunteeringSectionModel(createdVolunteeringSectionDto);
         return createdVolunteeringSection.save();
 
+    }
+    async deleteVolunteeringOpportunity(id: string): Promise<void> {
+        const deletedVolunteeringOpportunity = await this.volunteeringSectionModel.findOneAndDelete({ _id: id });
+        if (!deletedVolunteeringOpportunity) {
+            throw new NotFoundException(`Volunteering opportunity with ID ${id} not found`);
+        }
     }
 }
