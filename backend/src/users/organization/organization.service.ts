4445b1ba8c331d4845a351662ff4b7c7e4daba21
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-09 22:49:40 +0300

    added organizaiton service class

---
+++ backend/src/users/organization/organization.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { OrganizationDetail } from "src/schemas/organization_detail.schema";
+import { VolunteeringSection } from "src/schemas/volunteering_opportunity.schema";
+
+@Injectable()
+export class OrganizationService {
+    constructor(@InjectModel(OrganizationDetail.name) private organizationSectionModel: Model<OrganizationDetail>
+    ) { }
+}
