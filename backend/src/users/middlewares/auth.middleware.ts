ee7643b0315021a3f1c8e1c5b4081b294402557e
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-09 14:28:08 +0300

    added a return on teh failed authentication

--- backend/src/users/middlewares/auth.middleware.ts
+++ backend/src/users/middlewares/auth.middleware.ts
@@ -1,35 +1,38 @@
-import { Injectable, NestMiddleware, UnauthorizedException } from "@nestjs/common";
+import { Injectable, NestMiddleware } from "@nestjs/common";
 import { Request, Response, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
 import { UserDocument, UsersService } from "../user/users.service";
-import { Types } from "mongoose";
+import { Types,  } from "mongoose";
 
 export interface ExpressRequest extends Request {
-    user?: UserDocument;
+  user?: UserDocument;
 }
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-    constructor(private readonly userService: UsersService) { }
+  constructor(private readonly userService: UsersService) {}
 
-    async use(req: ExpressRequest, res: Response, next: NextFunction) {
-        if (!req.headers["authorization"]) {
-            throw new UnauthorizedException("Unauthorized");
-        }
-
-        const token = req.headers["authorization"].split(" ")[1];
-
-        try {
-            const decoded = verify(token, "JWT_SECRET") as { userId: Types.ObjectId };
-            if (!decoded) throw new UnauthorizedException("Unauthorized");
-            if (!Types.ObjectId.isValid(decoded.userId)) throw new UnauthorizedException("Unauthorized");
-
-            const user = await this.userService.findById(decoded.userId);
-            req.user = user;
-            next();
-        } catch (e) {
-            req.user = null;
-            throw new UnauthorizedException("Unauthorized");
-        }
+  async use(req: ExpressRequest, res: Response, next: NextFunction) {
+    if (!req.headers["authorization"]) {
+      return res.status(401).json({ message: "Unauthorized" });
     }
+
+    const token = req.headers["authorization"].split(" ")[1];
+
+    try {
+      const decoded = verify(token, "JWT_SECRET") as { userId: Types.ObjectId };
+      if (!decoded) {
+        return res.status(401).json({status:401, message: "Unauthorized" });
+      }
+      if (!Types.ObjectId.isValid(decoded.userId)) {
+        return res.status(401).json({status:401, message: "Unauthorized" });
+      }
+      const user = await this.userService.findById(decoded.userId);
+      req.user = user;
+      next();
+    } catch (e) {
+      req.user = null;
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+  }
 }
