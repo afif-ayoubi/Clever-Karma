4b695a90202e4da5c26b47ef717804533dd50e5a
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-09 21:22:15 +0300

    added section update servcie

--- backend/src/users/volunteering_section/volunteering_opportunity.service.ts
+++ backend/src/users/volunteering_section/volunteering_opportunity.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { VolunteeringSection } from "src/schemas/volunteering_opportunity.schema";
@@ -19,5 +19,11 @@ export class VolunteeringSectionService {
         if (!deletedVolunteeringOpportunity) {
             throw new NotFoundException(`Volunteering opportunity with ID ${id} not found`);
         }
+    }
+    async updateVolunteeringOpportunity(id: string, updatedVolunteeringSectionDto: CreatedVolunteeringSectionDto): Promise<VolunteeringSection> {
+        const updatedVolunteeringOpportunity = await this.volunteeringSectionModel.findByIdAndUpdate(id, updatedVolunteeringSectionDto, { new: true });
+        if (!updatedVolunteeringOpportunity) throw new HttpException("Section not found", HttpStatus.UNPROCESSABLE_ENTITY);
+
+        return updatedVolunteeringOpportunity;
     }
 }
