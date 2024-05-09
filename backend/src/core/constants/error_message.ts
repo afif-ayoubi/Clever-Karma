beffc991859bb0476205ce34afb79d8b9b82a249
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-10 01:08:16 +0300

    created constants errors

---
+++ backend/src/core/constants/error_message.ts
@@ -0,0 +1,10 @@
+export const ERROR_MESSAGES={
+    VOLUNTEERING_SECTION_REQUIRED:'VolunteeringSection is required',
+    INVALID_VOLUNTEERING_SECTION_VALUES:'Invalid VolunteeringSection values provided',
+    ORGANIZATION_NOT_FOUND: 'Organization not found',
+    EMAIL_ALREADY_EXISTS: 'Email already exists',
+    USER_NOT_FOUND: 'User not found',
+    INCORRECT_PASSWORD: 'incorrect password',
+    EMAIL_ALREADY_TAKEN: 'Email is already taken',
+
+}
