5db9d45c8af21807efa05a7cfea1f0f9c23fc3c5
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-10 17:56:07 +0300

    defined the thresholds and print the aspect of air quality

--- IOT/IOT.ino
+++ IOT/IOT.ino
@@ -1,8 +1,10 @@
 #include <DHT.h>
 #include "MQ135.h"
 
-#define DHTPIN 15  
+#define DHTPIN 15            
 #define DHTTYPE DHT22       
+
+#define MQ135_ANALOG_PIN 33 
 
 DHT dht(DHTPIN, DHTTYPE);
 MQ135 gasSensor = MQ135(MQ135_ANALOG_PIN);
@@ -22,10 +24,9 @@ void loop() {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
-  
-  float rzero = gasSensor.getRZero();
-  float ppm = gasSensor.getPPM();
 
+  float rzero = gasSensor.getRZero();
+    float ppm = gasSensor.getPPM();
 
   Serial.print("Humidity: ");
   Serial.print(humidity);
@@ -34,10 +35,25 @@ void loop() {
   Serial.print(temperature);
   Serial.println(" °C");
 
-  
+
   Serial.print("MQ-135 PPM: ");
   Serial.println(ppm);
 
+  const float goodThreshold = 400.0;
+  const float moderateThreshold = 1000.0;
+  const float poorThreshold = 2000.0;
+  const float veryPoorThreshold = 5000.0;
 
+  if (ppm <= goodThreshold) {
+    Serial.println("Air quality: Good");
+  } else if (ppm <= moderateThreshold) {
+    Serial.println("Air quality: Moderate");
+  } else if (ppm <= poorThreshold) {
+    Serial.println("Air quality: Poor");
+  } else if (ppm <= veryPoorThreshold) {
+    Serial.println("Air quality: Very Poor");
+  } else {
+    Serial.println("Air quality: Hazardous");
+  }
 
 }
