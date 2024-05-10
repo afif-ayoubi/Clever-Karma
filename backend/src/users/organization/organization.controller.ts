4ccf3940173d27f6fb508178c1c1920999e2011a
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-10 12:08:10 +0300

    adjusted the delete method

--- backend/src/users/organization/organization.controller.ts
+++ backend/src/users/organization/organization.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
 import { OrganizationService } from "./organization.service";
 import { OrganizationDto } from "./dto/organization.dto";
 import { OrganizationDetail } from "src/schemas/organization_detail.schema";
@@ -16,7 +16,7 @@ import { SUCCESS_METHODS } from "src/core/constants/success_message";
         return this.organizationService.getAllOrganizations();
     }
     @Delete(':id')
-    async deleteOrganization(@Body() id: string): Promise<{message:string}> {
+    async deleteOrganization(@Param('id') id: string): Promise<{message:string}> {
         await  this.organizationService.deleteOrganization(id);
         return {message: SUCCESS_METHODS.ORGANIZATION_DELETED};
     }
