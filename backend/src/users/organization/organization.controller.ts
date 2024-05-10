5a7274bf8e2dc768bebe36874571698719c7f638
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-10 11:55:24 +0300

    adjusted the return success delete

--- backend/src/users/organization/organization.controller.ts
+++ backend/src/users/organization/organization.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Delete, Get, Post } from "@nestjs/common";
 import { OrganizationService } from "./organization.service";
 import { OrganizationDto } from "./dto/organization.dto";
 import { OrganizationDetail } from "src/schemas/organization_detail.schema";
+import { SUCCESS_METHODS } from "src/core/constants/success_message";
 
 @Controller('organization')
  export class OrganizationController {
@@ -17,6 +18,6 @@ import { OrganizationDetail } from "src/schemas/organization_detail.schema";
     @Delete(':id')
     async deleteOrganization(@Body() id: string): Promise<{message:string}> {
         await  this.organizationService.deleteOrganization(id);
-        return {message: 'Organization deleted successfully'};
+        return {message: SUCCESS_METHODS.ORGANIZATION_DELETED};
     }
  }
