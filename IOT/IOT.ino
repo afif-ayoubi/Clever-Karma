f48c53fa30d8b786755ee54e1e0ec6828c622b51
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-10 17:43:41 +0300

    initilized the project with and print the value of DHT22

---
+++ IOT/IOT.ino
@@ -0,0 +1,35 @@
+#include <DHT.h>
+
+#define DHTTYPE DHT22       
+
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+  delay(2000); 
+
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+
+
+
+}
