708179c6fe286be02b170b3e758927ff4fbbdd11
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-10 22:44:58 +0300

    initilized firebase and wifi connectivity

--- IOT/IOT.ino
+++ IOT/IOT.ino
@@ -1,5 +1,14 @@
 #include <DHT.h>
 #include "MQ135.h"
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+
+#define WIFI_SSID "TurkTelekom_TP8DC2_2.4GHz"
+#define WIFI_PASSWORD "UaDADAnuNR3u"
+#define API_KEY "AIzaSyDPRwvbtTPWoZoMuZiXMwePaJHsbIuwzf8"
+#define DATABASE_URL "https://clever-karma-152f3-default-rtdb.europe-west1.firebasedatabase.app/"
 
 #define DHTPIN 15            
 #define DHTTYPE DHT22       
@@ -7,11 +16,22 @@
 #define MQ135_ANALOG_PIN 33 
 #define MQ2_ANALOG_PIN 34
 
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+bool signup= false;
+int ldrData=0;
+float volatage=0.0;
+
 DHT dht(DHTPIN, DHTTYPE);
 MQ135 gasSensor = MQ135(MQ135_ANALOG_PIN);
 
 void setup() {
   Serial.begin(115200);
+  WiFi.begin(WIFI_SSID,WIFI_PASSWORD);
+  Serial.print("Connecting to Wifi")
   dht.begin();
 }
 
