6974f6f69b929b37980d16de31ceb87794bf0d40
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-10 11:46:08 +0300

    added the get all controllers

--- backend/src/users/organization/organization.controller.ts
+++ backend/src/users/organization/organization.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { OrganizationService } from "./organization.service";
 import { OrganizationDto } from "./dto/organization.dto";
 import { OrganizationDetail } from "src/schemas/organization_detail.schema";
@@ -9,5 +9,9 @@ import { OrganizationDetail } from "src/schemas/organization_detail.schema";
     @Post('')
     async createOrganization(@Body() organizationDto: OrganizationDto): Promise<OrganizationDetail> {
         return this.organizationService.createOrganization(organizationDto);
+    }
+    @Get('')
+    async getOrganizations(): Promise<OrganizationDetail[]> {
+        return this.organizationService.getAllOrganizations();
     }
  }
