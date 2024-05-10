f8051721d50eff2d72774cf02c52e6eb0c6c5278
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-10 15:29:38 +0300

    removed the name from the model

--- backend/src/schemas/follower_schema.ts
+++ backend/src/schemas/follower_schema.ts
@@ -5,8 +5,5 @@ import { Types } from 'mongoose';
 export class Follower{
     @Prop({ required: true })
     organizationId: Types.ObjectId;
-
-    @Prop({ required: true })
-    organizationName: string;
 }
 export const FollowerSchema = SchemaFactory.createForClass(Follower);
