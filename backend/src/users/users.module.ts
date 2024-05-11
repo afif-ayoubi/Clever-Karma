0ed79e22983bd3c71738bca1c14c8e8c3c636e9a
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-11 17:46:54 +0300

    removed unused schemas

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -4,10 +4,6 @@ import { User, UserSchema } from "src/schemas/user.schema";
 import { UsersService } from "./user/users.service";
 import { UsersController } from "./user/users.controller";
 import { NotificationSchema, Notifications } from "src/schemas/notification.schema";
-import { UserInfo, UserInfoSchema } from "src/schemas/user_info.schema";
-import { OrganizationDetail, OrganizationDetailSchema } from "src/schemas/organization_detail.schema";
-import { UserLocation, UserLocationSchema } from "src/schemas/location.schema";
-import { Follower, FollowerSchema } from "src/schemas/follower_schema";
 import { VolunteeringSection, VolunteeringSectionSchema } from "src/schemas/volunteering_opportunity.schema";
 import { VolunteeringSectionService } from "./volunteering_section/volunteering_section.service";
 import { OrganizationController } from "./organization/organization.controller";
@@ -25,22 +21,8 @@ import { VolunteeringSectionController } from "./volunteering_section/volunteeri
                 name: Notifications.name,
                 schema: NotificationSchema,
             },
-            {
-                name: UserInfo.name,
-                schema: UserInfoSchema,
-            },
-            {
-                name: OrganizationDetail.name,
-                schema: OrganizationDetailSchema,
-            },
-            {
-                name: UserLocation.name,
-                schema: UserLocationSchema,
-            },
-            {
-                name: Follower.name,
-                schema: FollowerSchema,
-            },
+
+
             {
                 name: VolunteeringSection.name,
                 schema: VolunteeringSectionSchema,
@@ -49,7 +31,7 @@ import { VolunteeringSectionController } from "./volunteering_section/volunteeri
     ],
     providers: [
         UsersService, VolunteeringSectionService, OrganizationService],
-    controllers: [UsersController,VolunteeringSectionController, OrganizationController],
+    controllers: [UsersController, VolunteeringSectionController, OrganizationController],
     exports: [UsersService]
 })
 export class UserModule { }
