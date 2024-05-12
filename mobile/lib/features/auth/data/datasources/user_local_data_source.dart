5e15712ecf255876fb1c022e286fe614bfba1de3
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-12 17:04:25 +0300

    implemented the cashed user method

--- mobile/lib/features/auth/data/datasources/user_local_data_source.dart
+++ mobile/lib/features/auth/data/datasources/user_local_data_source.dart
@@ -1,3 +1,35 @@
+import 'package:dartz/dartz.dart';
+import 'package:hive/hive.dart';
+import 'package:mobile/core/error/exception.dart';
+import 'package:mobile/features/auth/domain/entities/user.dart';
+
+import '../../../../core/constants/constants.dart';
+import '../models/user_model.dart';
+
 abstract class UserLocalDataSource {
+  Future<UserModel> getCachedUser();
+
+  Future<Unit> cacheUser(UserModel userModel);
+}
+
+final _userBox = Hive.box<UserModel>(USER_BOX);
+const CACHED_USER = "CACHED_USER";
+
+class UserLocalDataSourceImpl implements UserLocalDataSource {
+  @override
+  Future<Unit> cacheUser(UserModel userModel) async {
+    await _userBox.put(CACHED_USER, userModel);
+    print(
+        'User Cached ${userModel} in "${_userBox.name}". Length: ${_userBox.length}');
+    return Future.value(unit);
+  }
+
+  @override
+  Future<UserModel> getCachedUser() {
+ 
+  }
+
 
 }
+
+
