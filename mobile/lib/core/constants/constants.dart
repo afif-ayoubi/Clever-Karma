424e2c0fdf82d69374d2497d1b39ce7746568f65
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-12 17:03:29 +0300

    initialized hive iniside our app

---
+++ mobile/lib/core/constants/constants.dart
@@ -0,0 +1,2 @@
+const BASE_URL = "http://localhost:3000";
+const USER_BOX = "user";
--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,16 +1,28 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'core/constants/constants.dart';
+import 'features/auth/domain/entities/user.dart';
 import 'features/opportunity/core/services/notifications.dart';
 import 'injection_container.dart' as di;
 import 'main_application.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'firebase_options.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,);
-
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await NotificationService.instance.initNotifications();
+  Directory dir = await getApplicationDocumentsDirectory();
+
+  await Hive.initFlutter(dir.path);
+  Hive.registerAdapter(UserAdapter());
+  await Hive.openBox<User>(USER_BOX);
   FlutterNativeSplash.remove();
 
   await di.init();
