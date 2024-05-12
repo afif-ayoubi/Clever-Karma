f1288f497571e5aa2c3e14999488ef3dca538aa7
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-12 11:31:42 +0300

    added try and catch for each one

--- mobile/lib/features/auth/data/datasources/user_remote_data_source.dart
+++ mobile/lib/features/auth/data/datasources/user_remote_data_source.dart
@@ -8,49 +8,72 @@ import '../../../../core/error/exception.dart';
 
 abstract class UserRemoteDataSource {
   Future<Unit> createUser(UserModel user);
+
   Future<Unit> login(UserModel user);
+
   Future<Unit> logout();
+
   Future<Unit> updateUser(UserModel user);
+
   Future<UserModel> getUser();
 }
-const BASE_URL="http://localhost:3000";
+
+const BASE_URL = "http://localhost:3000";
+
 class UserRemoteDataSourceImpl implements UserRemoteDataSource {
   final http.Client client;
+
   UserRemoteDataSourceImpl({required this.client});
+
   @override
   Future<Unit> createUser(UserModel user) async {
     final body = user.toJson();
-    final response = await client.post(Uri(path: '$BASE_URL/user'), body: body);
-    if (response.statusCode == 201) {
-      return Future.value(unit);
-    }else{
-      throw ServerException();
+    try {
+      final response = await client.post(Uri(path: '$BASE_URL/user'), body: body);
+      if (response.statusCode == 201) {
+        return Future.value(unit);
+      } else {
+        throw ServerException();
+      }
+    } catch (e) {
+      print("Caught an exception: $e");
+      throw e;
     }
   }
 
   @override
   Future<UserModel> getUser() async {
-    final response = await client.get(
-      Uri.parse(BASE_URL + "/user"),
-      headers: {"Content-Type": "application/json"},
-    );
-    if (response.statusCode == 200) {
-      final decodedJson = json.decode(response.body);
-      final UserModel userModel = UserModel.fromJson(decodedJson);
-      return userModel;
-    }else{
-      throw ServerException();
+    try {
+      final response = await client.get(
+        Uri.parse(BASE_URL + "/user"),
+        headers: {"Content-Type": "application/json"},
+      );
+      if (response.statusCode == 200) {
+        final decodedJson = json.decode(response.body);
+        final UserModel userModel = UserModel.fromJson(decodedJson);
+        return userModel;
+      } else {
+        throw ServerException();
+      }
+    } catch (e) {
+      print("Caught an exception: $e");
+      throw e;
     }
   }
 
   @override
   Future<Unit> login(UserModel user) async {
     final body = user.toJson();
-    final response = await client.post(Uri(path: '$BASE_URL/user'), body: body);
-    if (response.statusCode == 201) {
-      return Future.value(unit);
-    }else{
-      throw ServerException();
+    try {
+      final response = await client.post(Uri(path: '$BASE_URL/user'), body: body);
+      if (response.statusCode == 201) {
+        return Future.value(unit);
+      } else {
+        throw ServerException();
+      }
+    } catch (e) {
+      print("Caught an exception: $e");
+      throw e;
     }
   }
 
