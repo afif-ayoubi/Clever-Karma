bd9fcfe6e809bce9a5adf045b552b0ce852023f8
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-12 17:03:01 +0300

    implemented hive on teh user class

--- mobile/lib/features/auth/domain/entities/user.dart
+++ mobile/lib/features/auth/domain/entities/user.dart
@@ -1,37 +1,47 @@
 import 'package:equatable/equatable.dart';
+import 'package:hive/hive.dart';
 
-class User extends Equatable {
+part 'user.g.dart';
+
+@HiveType(typeId: 0)
+class User extends HiveObject with EquatableMixin {
+  @HiveField(0)
   final int id;
+  @HiveField(1)
   final String firstName;
+  @HiveField(2)
   final String lastName;
+  @HiveField(3)
   final String email;
+  @HiveField(4)
   final String password;
+  @HiveField(5)
   final DateTime? dateOfBirth;
+  @HiveField(6)
   final String? gender;
+  @HiveField(7)
   final int? phoneNumber;
 
-
-  const User(
-      {required this.id,
-      required this.firstName,
-      required this.lastName,
-      required this.email,
-      required this.password,
-       this.dateOfBirth,
-       this.phoneNumber,
-       this.gender,
-      });
+  User({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.password,
+    this.dateOfBirth,
+    this.phoneNumber,
+    this.gender,
+  });
 
   @override
   List<Object?> get props => [
-        id,
-        firstName,
-        lastName,
-        email,
-        password,
-        dateOfBirth,
-        phoneNumber,
-        gender,
-
-      ];
+    id,
+    firstName,
+    lastName,
+    email,
+    password,
+    dateOfBirth,
+    phoneNumber,
+    gender,
+  ];
 }
