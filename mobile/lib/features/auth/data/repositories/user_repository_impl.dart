101fa7e624b090a141f2b28c4d8920ca5540e34b
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-12 17:23:25 +0300

    added a common return function

--- mobile/lib/features/auth/data/repositories/user_repository_impl.dart
+++ mobile/lib/features/auth/data/repositories/user_repository_impl.dart
@@ -3,22 +3,26 @@ import 'package:mobile/core/error/failures.dart';
 import 'package:mobile/core/network/network_info.dart';
 import 'package:mobile/features/auth/data/datasources/user_local_data_source.dart';
 import 'package:mobile/features/auth/data/datasources/user_remote_data_source.dart';
-// import 'package:mobile/features/auth/data/models/user_model.dart';
 import 'package:mobile/features/auth/domain/entities/user.dart';
 import 'package:mobile/features/auth/domain/repositories/user_repository.dart';
+import '../../../../core/error/exception.dart';
+import '../models/user_model.dart';
+
+typedef Future<Unit> UserMethods();
 
 class UsersRepositoryImpl implements UsersRepository {
   final UserRemoteDataSource remoteDataSource;
   final UserLocalDataSource localDataSource;
   final NetworkInfo networkInfo;
+
   UsersRepositoryImpl(
       {required this.localDataSource,
       required this.remoteDataSource,
       required this.networkInfo});
+
   @override
   Future<Either<Failure, Unit>> create(user) {
-   // final UserModel userModel = UserModel.fromUser(user);
-     throw UnimplementedError();
+   
   }
 
   @override
@@ -57,5 +61,16 @@ class UsersRepositoryImpl implements UsersRepository {
     throw UnimplementedError();
   }
 
- // Future<Either<Failure, Unit>> _getMessage(Fu) {}
+  Future<Either<Failure, Unit>> _getMessage(UserMethods userMethods) async {
+    if (await networkInfo.isConnected) {
+      try {
+        await userMethods();
+        return Right(unit);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      return Left(OfflineFailure());
+    }
+  }
 }
