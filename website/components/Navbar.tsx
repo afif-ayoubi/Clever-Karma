ac3a71f22b923ade139134a9beab47fd4649ae75
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-14 09:32:56 +0300

    added the logo

--- website/components/Navbar.tsx
+++ website/components/Navbar.tsx
@@ -1,9 +1,19 @@
+import Link from 'next/link'
 import React from 'react'
+import Image from "next/image"
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <nav className="flexBetween max-container padding-container relative z-30 py-5">
+
+            <Link href="/">
+                <Image src="/logo.png" alt="logo" width={74} height={29} />
+            </Link>
+        </nav>
+
+
+
+    )
 }
 
 export default Navbar
