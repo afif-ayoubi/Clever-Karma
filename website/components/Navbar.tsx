1f7e809e56db5b1d9c8f7caacc8d9f39e744bc29
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-14 09:35:35 +0300

    added button to the navbar

--- website/components/Navbar.tsx
+++ website/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
 import Image from "next/image"
+import Button from './Button'
 
 const Navbar = () => {
     return (
@@ -9,6 +10,15 @@ const Navbar = () => {
             <Link href="/">
                 <Image src="/logo.png" alt="logo" width={74} height={29} />
             </Link>
+            <div className="lg:flexCenter hidden">
+                <Button
+                    type="button"
+                    title="Login"
+                    icon="/user.svg"
+                    variant="btn_dark_green"
+                />
+            </div>
+
         </nav>
 
 
