108d482e0bf99b94ef5fbc79b7dbda89ade8f722
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-14 22:46:27 +0300

    added the last part

--- website/components/Footer.tsx
+++ website/components/Footer.tsx
@@ -7,12 +7,12 @@ const Footer = () => {
     <footer className="flexCenter mb-24">
       <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
-          <Link href="/" className="mb-10">
+          <Link href="/" >
             <span className="text-2xl font-bold">
               Clever <span className="text-green-50">Karma</span>
             </span>
           </Link>
-          <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
+          <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1 mt-1'>
             {FOOTER_LINKS.map((columns) => (
               <FooterColumn title={columns.title}>
                 <ul className="regular-14 flex flex-col gap-4 text-gray-30">
