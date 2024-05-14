cb062212b2ef251f4a45e466b9c27b9c720f73db
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-14 22:37:45 +0300

    added the name of the website

--- website/components/Footer.tsx
+++ website/components/Footer.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import Link from 'next/link'
 const Footer = () => {
   return (
     <footer className="flexCenter mb-24">
+      <div className="padding-container max-container flex w-full flex-col gap-14">
+        <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
+          <Link href="/" className="mb-10">
+            <span className="text-2xl font-bold">
+              Clever <span className="text-green-50">Karma</span>
+            </span>          </Link>
+        </div>
+      </div>
     </footer>
   )
 }
