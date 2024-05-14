867eecba6461a27c19bb9d7851f0bb49ef1c070b
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-14 22:41:40 +0300

    added the footer socials constants of the footer components

--- website/constants/index.ts
+++ website/constants/index.ts
@@ -54,11 +54,20 @@ export const FOOTER_LINKS = [
     },
   ];
   export const FOOTER_CONTACT_INFO = {
-    title: 'Contact Us',
+  title: 'Contact Us',
+  links: [
+    { label: 'Admin Officer', value: '123-456-7890' },
+    { label: 'Email Officer', value: 'hilink@akinthil.com' },
+  ],
+};
+export const SOCIALS = {
+    title: 'Social',
     links: [
-      { label: 'Admin Officer', value: '123-456-7890' },
-      { label: 'Email Officer', value: 'hilink@akinthil.com' },
+      '/facebook.svg',
+      '/instagram.svg',
+      '/twitter.svg',
+      '/youtube.svg',
+      '/wordpress.svg',
     ],
-  };
-  
+}
   
