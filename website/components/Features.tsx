7e1c3ad7dbb8c53e0fbcf2ba38f800077294fe4c
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-14 16:12:17 +0300

    created teh first part

--- website/components/Features.tsx
+++ website/components/Features.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
-
-const Features= () => {
+import Image from 'next/image'
+const Features = () => {
   return (
-    <div>Features</div>
+    <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="max-container padding-container relative w-full flex justify-end">
+        <div className="flex flex-1 lg:min-h-[900px]">
+          <Image
+            src="/phone.png"
+            alt="phone"
+            width={440}
+            height={1000}
+            className="feature-phone"
+          />
+        </div>
+      </div>
+    </section>
   )
 }
 
