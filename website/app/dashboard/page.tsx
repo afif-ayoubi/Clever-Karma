b4021f23965fc166b0c40b322c400bdbbb0bb768
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-15 22:46:56 +0300

    adjusted the card

--- website/app/dashboard/page.tsx
+++ website/app/dashboard/page.tsx
@@ -1,18 +1,17 @@
-'use client'
-import React from 'react'
-import '../ui/global.css'
 import Card from "../ui/dashboard/card/card";
 import Chart from "../ui/dashboard/chart/chart";
 import styles from "../ui/dashboard/dashboard.module.css";
 import Rightbar from "../ui/dashboard/rightbar/rightbar";
 import Transactions from "../ui/dashboard/transactions/transactions";
+
 const Dashboard = () => {
   return (
-    <div>
-      <div className={styles.cards}>
-        <Card />
-        <Card />
-        <Card />
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          {cards.map((item) => (
+            <Card item={item} key={item.id} />
+          ))}
         </div>
         <Transactions />
         <Chart />
