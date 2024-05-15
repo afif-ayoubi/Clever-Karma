0e348b28ea59eb2b92c905426d6c3fda8df9fb2d
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-15 22:50:55 +0300

    added dummy data

---
+++ website/app/lib/data.js
@@ -0,0 +1,22 @@
+// DUMMY DATA
+
+export const cards = [
+    {
+      id: 1,
+      title: "Total Users",
+      number: 10,
+      change: 12,
+    },
+    {
+      id: 2,
+      title: "Total Organizations",
+      number: 12,
+      change: -2,
+    },
+    {
+      id: 3,
+      title: "Totoal Volunteering Opportunities",
+      number: 3,
+      change: 18,
+    },
+  ];
