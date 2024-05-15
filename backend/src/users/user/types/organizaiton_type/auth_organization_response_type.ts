ed3123ed01604e5e35ffc38be351b0235cd9dfce
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-15 09:01:20 +0300

    removed unnecessary attribute

--- backend/src/users/user/types/organizaiton_type/auth_organization_response_type.ts
+++ backend/src/users/user/types/organizaiton_type/auth_organization_response_type.ts
@@ -1,8 +1,6 @@
 import { Types } from 'mongoose';
-import { OrganizationDetail } from 'src/schemas/organization_detail.schema';
 import { User } from "src/schemas/user.schema";
 
 export type OrganizationAuthResponseType = Omit<User, 'password'> & {
   id: Types.ObjectId,
-  organizationDetail: OrganizationDetail 
 };
