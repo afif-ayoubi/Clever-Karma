6fb9af2750ebc5c329732a5bd9f0adca16e56b51
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-15 15:43:24 +0300

    added navbar and footer

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,18 +1,25 @@
+'use client'
+
 import Features from "@/components/Features";
+import Footer from "@/components/Footer";
 import GetApp from "@/components/GetApp";
 import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
 import Locations from "@/components/Locations";
-
+import Navbar from "@/components/Navbar";
+import { useRouter } from "next/navigation";
 export default function Home() {
+  
   return (
     <>
-    <Hero></Hero>
-    <Locations></Locations>
-    <Guide></Guide>
-    <Features></Features>
-    <GetApp></GetApp>
+      <Navbar />
+      <Hero></Hero>
+      <Locations></Locations>
+      <Guide></Guide>
+      <Features></Features>
+      <GetApp></GetApp>
+      <Footer />
     </>
-    
+
   );
 }
