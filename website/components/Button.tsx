871c31fdf2012433cac56a01c516255d81bb5721
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-15 15:42:58 +0300

    added navigation method

--- website/components/Button.tsx
+++ website/components/Button.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 type ButtonProps = {
   type: 'button' | 'submit';
@@ -6,15 +7,23 @@ type ButtonProps = {
   icon?: string;
   variant: string;
   full?: boolean;
-  destination: string; 
-  onClick?: () => void;
+  destination: string;
+
 }
 
-const Button = ({ type, title, icon, variant, full,destination, onClick }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full, destination }: ButtonProps) => {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push(destination);
+
+
+  }
+
   return (
     <button
-    className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
-      type={type}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
+      type={type} 
+      onClick={handleClick}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
       <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
