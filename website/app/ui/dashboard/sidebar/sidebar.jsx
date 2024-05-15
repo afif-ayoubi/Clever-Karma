a426db3c8b3524de97cf4c88ed2ea71743aac41c
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-15 20:06:41 +0300

    finilized the sidebar

--- website/app/ui/dashboard/sidebar/sidebar.jsx
+++ website/app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from "./sidebar.module.css";
 import Image from 'next/image';
+import MenuLink from './menuLink/menuLink';
 import {
     MdDashboard,
     MdSupervisedUserCircle,
@@ -79,7 +80,37 @@ import {
 const Sidebar =  () => {
     return (
       <div className={styles.container}>
+        <div className={styles.user}>
+          <Image
+            className={styles.userImage}
+            src={ "/noavatar.png"}
+            alt=""
+            width="50"
+            height="50"
+          />
+          <div className={styles.userDetail}>
+            <span className={styles.username}>{"Afif"}</span>
+            <span className={styles.userTitle}>Administrator</span>
+          </div>
+        </div>
+        <ul className={styles.list}>
+          {menuItems.map((cat) => (
+            <li key={cat.title}>
+              <span className={styles.cat}>{cat.title}</span>
+              {cat.list.map((item) => (
+                <MenuLink item={item} key={item.title} />
+              ))}
+            </li>
+          ))}
+        </ul>
+        <form
        
+        >
+          <button className={styles.logout}>
+            <MdLogout />
+            Logout
+          </button>
+        </form>
       </div>
     );
   };
