aa0480d7877ab0b6a89c78ec7df4c610488de2e4
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-16 12:02:45 +0300

    added one attribute

--- mobile/lib/features/opportunity/domain/entities/opportunity.dart
+++ mobile/lib/features/opportunity/domain/entities/opportunity.dart
@@ -4,16 +4,17 @@ class Opportunity {
   final int id;
   final String name;
   final String imageUrl;
+  final String description;
 
   const Opportunity(
-      {required this.id, required this.name, required this.imageUrl});
+      {required this.id, required this.name, required this.imageUrl, required this.description});
 
   static List<Opportunity> fakeData = [
     const Opportunity(
-        id: 1, name: 'blood Donation', imageUrl: AppImages.imgImg1),
+        id: 1, name: 'blood Donation', imageUrl: AppImages.imgImg1, description: ''),
     const Opportunity(
-        id: 2, name: 'blood Section', imageUrl: AppImages.onBoardingImg3),
+        id: 2, name: 'blood Section', imageUrl: AppImages.onBoardingImg3, description: ''),
     const Opportunity(
-        id: 3, name: 'blood Room', imageUrl: AppImages.onBoardingImg1),
+        id: 3, name: 'blood Room', imageUrl: AppImages.onBoardingImg1, description: ''),
   ];
 }
