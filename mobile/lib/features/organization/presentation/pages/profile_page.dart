a450d9548ff15c0b4dc9a4af49c457a0a2eff296
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-16 10:26:36 +0300

    initialized the org profile page

---
+++ mobile/lib/features/organization/presentation/pages/profile_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OrganizationProfilePage extends StatefulWidget {
+  const OrganizationProfilePage({super.key});
+
+  @override
+  State<OrganizationProfilePage> createState() => _OrganizationProfilePageState();
+}
+
+class _OrganizationProfilePageState extends State<OrganizationProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
