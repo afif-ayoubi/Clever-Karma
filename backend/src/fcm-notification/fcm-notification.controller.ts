d0266c00c240ee6c3313dc1231c2cb525a60d104
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-16 22:42:25 +0300

    created send notification

--- backend/src/fcm-notification/fcm-notification.controller.ts
+++ backend/src/fcm-notification/fcm-notification.controller.ts
@@ -2,33 +2,15 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { FcmNotificationService } from './fcm-notification.service';
 import { CreateFcmNotificationDto } from './dto/create-fcm-notification.dto';
 import { UpdateFcmNotificationDto } from './dto/update-fcm-notification.dto';
-
-@Controller('fcm-notification')
+@Controller('firebase')
 export class FcmNotificationController {
-  constructor(private readonly fcmNotificationService: FcmNotificationService) {}
+  constructor(
+    private readonly sendingNotificationService: FcmNotificationService,
+  ) { }
 
-  @Post()
-  create(@Body() createFcmNotificationDto: CreateFcmNotificationDto) {
-    return this.fcmNotificationService.create(createFcmNotificationDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.fcmNotificationService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.fcmNotificationService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFcmNotificationDto: UpdateFcmNotificationDto) {
-    return this.fcmNotificationService.update(+id, updateFcmNotificationDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.fcmNotificationService.remove(+id);
+  @Post('send-notification')
+  async sendNotidication(@Body() body: { token: string }) {
+    const { token } = body
+    return await this.sendingNotificationService.sendingNotificationOneUser(token)
   }
 }
