f5f1536c0d86ba0100298c632faa18eb71673e33
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-16 11:33:48 +0300

    made the value ready to put it inside graph

--- mobile/lib/features/organization/presentation/pages/graph_page.dart
+++ mobile/lib/features/organization/presentation/pages/graph_page.dart
@@ -1,42 +1,96 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mobile/core/extensions/text_theme.dart';
 import 'package:mobile/core/wdigets/loading_widget.dart';
 
 class GraphPage extends StatefulWidget {
-  const GraphPage({super.key});
+  const GraphPage({Key? key}) : super(key: key);
 
   @override
   State<GraphPage> createState() => _GraphPageState();
 }
 
 class _GraphPageState extends State<GraphPage> {
-  Query dbRef = FirebaseDatabase.instance.ref('');
+  late Query dbRef;
+  List<double> humidityList = [];
+  List<int> temperatureList = [];
+  List<double> ppmList = [];
+  List<int> mq2ValueList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    dbRef = FirebaseDatabase.instance.ref();
+
+    dbRef.onChildAdded.listen((event) {
+      _updateLists(event.snapshot.value);
+    });
+
+    dbRef.onChildChanged.listen((event) {
+      _updateLists(event.snapshot.value);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FirebaseAnimatedList(
-          query: dbRef,
-          itemBuilder: (BuildContext context, DataSnapshot snapshot,
-              Animation<double> animation, int index) {
-            if (!snapshot.exists || snapshot.value == null)
-              return LoadingWidget();
-            print(snapshot.value);
-            Map data = snapshot.value as Map;
-            double humidity = data['humidity'];
-            double temperature = data['temperature'];
-            double ppm = data['ppm'];
-            int mq2Value = data['mq2Value'];
+      backgroundColor: Colors.transparent,
+      body: SafeArea(child: _buildWidget(dbRef: dbRef)),
+    );
+  }
 
-            return ListTile(
-              title: Text('Humidity: $humidity'),
-              subtitle: Text(
-                  'Temperature: $temperature\nPPM: $ppm\nMQ2 Value: $mq2Value'),
-            );
-          },
-        ),
+  void _updateLists(dynamic data) {
+    if (data is Map<dynamic, dynamic>) {
+      setState(() {
+        humidityList.add(data['humidity']);
+        temperatureList.add(data['temperature']);
+        ppmList.add(data['ppm']);
+        mq2ValueList.add(data['mq2Value']);
+
+        if (humidityList.length > 5) humidityList.removeAt(0);
+        if (temperatureList.length > 5) temperatureList.removeAt(0);
+        if (ppmList.length > 5) ppmList.removeAt(0);
+        if (mq2ValueList.length > 5) mq2ValueList.removeAt(0);
+      });
+    }
+  }
+}
+
+class _buildWidget extends StatelessWidget {
+  const _buildWidget({
+    super.key,
+    required this.dbRef,
+  });
+
+  final Query dbRef;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30).r,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "DashBoard",
+            style: context.displayMedium,
+          ),
+          Expanded(
+            child: FirebaseAnimatedList(
+              query: dbRef,
+              itemBuilder: (BuildContext context, DataSnapshot snapshot,
+                  Animation<double> animation, int index) {
+                if (!snapshot.exists || snapshot.value == null)
+                  return LoadingWidget();
+
+                return SizedBox();
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
