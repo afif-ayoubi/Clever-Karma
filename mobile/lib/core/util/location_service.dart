8fdcfb4e52ebd1d0aa70b12d1882cd447e1baf65
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-16 14:11:44 +0300

    added location service class

---
+++ mobile/lib/core/util/location_service.dart
@@ -0,0 +1,35 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class LocationService {
+  static Future<LatLng?> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      throw Exception('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        throw Exception('Location permissions are denied');
+      }
+    }
+
+    final location = await Geolocator.getCurrentPosition();
+    final currentPosition = LatLng(location.latitude, location.longitude);
+    return currentPosition;
+  }
+}
