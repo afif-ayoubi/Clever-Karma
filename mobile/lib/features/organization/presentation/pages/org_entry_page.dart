5b9217adc99f9a1301da0c9d3e1f244ecf19964e
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-16 10:45:22 +0300

    implemented the bottom nav bar

--- mobile/lib/features/organization/presentation/pages/org_entry_page.dart
+++ mobile/lib/features/organization/presentation/pages/org_entry_page.dart
@@ -1,5 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/core/theme/hex_color.dart';
+import 'package:mobile/core/common_domain/entities/rive_asset.dart';
+import 'package:rive/rive.dart';
 
+import '../../../../core/util/rive_utils.dart';
+import '../widgets/org_entry_page/animated_bar.dart';
+import 'graph_page.dart';
+import 'org_profile_page.dart';
+List<RiveAsset> bottomNavs = [
+  RiveAsset(
+      artboard: "HOME",
+      stateMacineName: "HOME_interactivity",
+      title: "Home",
+      src: 'assets/RiveAssets/icons.riv',
+      number: 0),
+  // RiveAsset(
+  //     artboard: "SEARCH",
+  //     stateMacineName: "SEARCH_Interactivity",
+  //     title: "Search",
+  //     src: 'assets/RiveAssets/icons.riv',
+  //     number: 1),
+  RiveAsset(
+      artboard: "TIMER",
+      stateMacineName: "TIMER_Interactivity",
+      title: "Timer",
+      src: 'assets/RiveAssets/icons.riv',
+      number: 1),
+  RiveAsset(
+      artboard: "USER",
+      stateMacineName: "USER_Interactivity",
+      title: "Profile",
+      src: 'assets/RiveAssets/icons.riv',
+      number: 2),
+];
 class OrgEntryPage extends StatefulWidget {
   const OrgEntryPage({super.key});
 
@@ -7,11 +40,117 @@ class OrgEntryPage extends StatefulWidget {
   State<OrgEntryPage> createState() => _OrgEntryPageState();
 }
 
-class _OrgEntryPageState extends State<OrgEntryPage> {
+class _OrgEntryPageState extends State<OrgEntryPage>
+    with SingleTickerProviderStateMixin {
+  RiveAsset selectedBottomNav = bottomNavs.first;
+
+  late Animation<double> animation;
+  late AnimationController _animationController;
+  int pageIndex = 0;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    )..addListener(() {
+        setState(() {});
+      });
+    animation = Tween<double>(begin: 0, end: 1).animate(CurvedAnimation(
+        parent: _animationController, curve: Curves.fastOutSlowIn));
+    super.initState();
+  }
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+  List pages=[
+    GraphPage(),
+    OrganizationProfilePage(),
+    OrganizationProfilePage(),
+
+
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-
+    return Scaffold(
+      body: SafeArea(
+        child: pages[pageIndex],
+      
+      ),
+      bottomNavigationBar: Transform.translate(
+        offset: Offset(0, 100 * animation.value),
+        child: SafeArea(
+          child: Material(
+            color: Colors.transparent,
+            borderRadius: const BorderRadius.all(Radius.circular(24)),
+            child: Container(
+              padding: const EdgeInsets.all(12),
+              margin: const EdgeInsets.symmetric(horizontal: 24),
+              decoration: const BoxDecoration(
+                  color: HexColor.primaryColor,
+                  borderRadius: BorderRadius.all(Radius.circular(24))),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ...List.generate(
+                      bottomNavs.length,
+                      (index) => GestureDetector(
+                            onTap: () {
+                              bottomNavs[index].input!.change(true);
+                              if (bottomNavs[index] != selectedBottomNav) {
+                                setState(() {
+                                  selectedBottomNav = bottomNavs[index];
+                                });
+                              }
+                              Future.delayed(const Duration(seconds: 1), () {
+                                bottomNavs[index].input!.change(false);
+                              });
+                              setState(() {
+                                pageIndex = bottomNavs[index].number ?? 0;
+                              });
+                            },
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                AnimatedBar(
+                                  isActive:
+                                      bottomNavs[index] == selectedBottomNav,
+                                ),
+                                SizedBox(
+                                  height: 36,
+                                  width: 36,
+                                  child: Opacity(
+                                    opacity:
+                                        bottomNavs[index] == selectedBottomNav
+                                            ? 1
+                                            : 0.5,
+                                    child: RiveAnimation.asset(
+                                      bottomNavs[index].src,
+                                      artboard: bottomNavs[index].artboard,
+                                      onInit: (artboard) {
+                                        StateMachineController controller =
+                                            RiveUtils.getRiveController(
+                                                artboard,
+                                                stateMachineName:
+                                                    bottomNavs[index]
+                                                        .stateMacineName);
+                                        bottomNavs[index].input = controller
+                                            .findSMI("active") as SMIBool;
+                                      },
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ))
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
