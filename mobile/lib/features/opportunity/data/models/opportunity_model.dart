b9cc978c6f3dd857b02e624dc103778b78931ab2
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-18 19:33:41 +0300

    added model

---
+++ mobile/lib/features/opportunity/data/models/opportunity_model.dart
@@ -0,0 +1,27 @@
+import '../../domain/entities/opportunity.dart';
+
+class OpportunityModel extends Opportunity {
+  OpportunityModel({
+    super.id,
+    required super.name,
+    required super.imageUrl,
+    required super.description,
+  });
+
+  factory OpportunityModel.fromJson(Map<String, dynamic> json) {
+    return OpportunityModel(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
+      imageUrl: json['imageUrl'],
+    );
+  }
+
+  OpportunityModel.fromOpportunity(Opportunity opportunity)
+      : super(
+          id: opportunity.id,
+          name: opportunity.name,
+          imageUrl: opportunity.imageUrl,
+          description: opportunity.description,
+        );
+}
