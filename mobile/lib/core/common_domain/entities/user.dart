0d343f03b65c3df5eaa4929aa122693bc20055fb
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-18 12:36:27 +0300

    removed hive

--- mobile/lib/core/common_domain/entities/user.dart
+++ mobile/lib/core/common_domain/entities/user.dart
@@ -2,7 +2,7 @@ import 'package:equatable/equatable.dart';
 import 'package:hive/hive.dart';
 
 import '../../../features/auth/domain/entities/volunteer.dart';
-
+@HiveType(typeId: 1)
 class Volunteer extends HiveObject with EquatableMixin {
   @HiveField(0)
   final User user;
