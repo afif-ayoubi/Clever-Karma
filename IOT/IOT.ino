1447c9f078de639ce8f01cc22f396c32a2bd7e03
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-18 03:41:29 +0300

    changed the wifi adn password

--- IOT/IOT.ino
+++ IOT/IOT.ino
@@ -5,8 +5,8 @@
   #include "addons/TokenHelper.h"
   #include "addons/RTDBHelper.h"
 
-  #define WIFI_SSID "TurkTelekom_TP8DC2_2.4GHz"
-  #define WIFI_PASSWORD "UaDADAnuNR3u"
+  #define WIFI_SSID "SE Factory"
+  #define WIFI_PASSWORD "SE-F@tory1243%"
   #define API_KEY "AIzaSyDPRwvbtTPWoZoMuZiXMwePaJHsbIuwzf8"
   #define DATABASE_URL "https://clever-karma-152f3-default-rtdb.europe-west1.firebasedatabase.app/"
 
@@ -81,59 +81,59 @@
       Serial.println("Sensor data pushed to Firebase");
     }
 
-    // delay(2000); 
+    delay(2000); 
 
-    //   float humidity = dht.readHumidity();
-    //   float temperature = dht.readTemperature();
-    //   float rzero = gasSensor.getRZero();
-    //   float ppm = gasSensor.getPPM();
-    //   int mq2Value = analogRead(MQ2_ANALOG_PIN);
-    // if (isnan(humidity) || isnan(temperature)) {
-    //   Serial.println("Failed to read from DHT sensor!");
-    //   return;
-    // }
-
-
-
-    // Serial.print("Humidity: ");
-    // Serial.print(humidity);
-    // Serial.print(" %\t");
-    // Serial.print("Temperature: ");
-    // Serial.print(temperature);
-    // Serial.println(" °C");
+      float humidity = dht.readHumidity();
+      float temperature = dht.readTemperature();
+      float rzero = gasSensor.getRZero();
+      float ppm = gasSensor.getPPM();
+      int mq2Value = analogRead(MQ2_ANALOG_PIN);
+    if (isnan(humidity) || isnan(temperature)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
 
 
-    // Serial.print("MQ-135 PPM: ");
-    // Serial.println(ppm);
-    // Serial.print("MQ-2 value: ");
-    // Serial.println(mq2Value);
 
-    // const float goodThreshold = 400.0;
-    // const float moderateThreshold = 1000.0;
-    // const float poorThreshold = 2000.0;
-    // const float veryPoorThreshold = 5000.0;
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print(" %\t");
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println(" °C");
 
-    // if (ppm <= goodThreshold) {
-    //   Serial.println("Air quality: Good");
-    // } else if (ppm <= moderateThreshold) {
-    //   Serial.println("Air quality: Moderate");
-    // } else if (ppm <= poorThreshold) {
-    //   Serial.println("Air quality: Poor");
-    // } else if (ppm <= veryPoorThreshold) {
-    //   Serial.println("Air quality: Very Poor");
-    // } else {
-    //   Serial.println("Air quality: Hazardous");
-    // }
 
-    // const int cleanAirValue = 700; 
-    // const int smokeDetectedValue = 500; 
+    Serial.print("MQ-135 PPM: ");
+    Serial.println(ppm);
+    Serial.print("MQ-2 value: ");
+    Serial.println(mq2Value);
 
-    // if (mq2Value >= cleanAirValue) {
-    //   Serial.println("Smoke: No smoke detected");
-    // } else if (mq2Value <= smokeDetectedValue) {
-    //   Serial.println("Smoke: Smoke detected!");
-    // } else {
-    //   Serial.println("Smoke: Possible smoke detected");
-    // }
+    const float goodThreshold = 400.0;
+    const float moderateThreshold = 1000.0;
+    const float poorThreshold = 2000.0;
+    const float veryPoorThreshold = 5000.0;
+
+    if (ppm <= goodThreshold) {
+      Serial.println("Air quality: Good");
+    } else if (ppm <= moderateThreshold) {
+      Serial.println("Air quality: Moderate");
+    } else if (ppm <= poorThreshold) {
+      Serial.println("Air quality: Poor");
+    } else if (ppm <= veryPoorThreshold) {
+      Serial.println("Air quality: Very Poor");
+    } else {
+      Serial.println("Air quality: Hazardous");
+    }
+
+    const int cleanAirValue = 700; 
+    const int smokeDetectedValue = 500; 
+
+    if (mq2Value >= cleanAirValue) {
+      Serial.println("Smoke: No smoke detected");
+    } else if (mq2Value <= smokeDetectedValue) {
+      Serial.println("Smoke: Smoke detected!");
+    } else {
+      Serial.println("Smoke: Possible smoke detected");
+    }
 
   }
