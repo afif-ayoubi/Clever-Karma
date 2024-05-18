97d3e7f52483606d313ed53d13c7f1aa86c309dd
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-18 17:31:32 +0300

    retrieved only the new tokens

--- backend/src/fcm-notification/fcm-notification.service.ts
+++ backend/src/fcm-notification/fcm-notification.service.ts
@@ -11,7 +11,7 @@ admin.initializeApp({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  
+
   }),
 });
 
@@ -20,7 +20,7 @@ export class FcmNotificationService {
   constructor(
     @InjectModel(Notifications.name) private notificationModel: Model<Notifications>,
     @InjectModel(User.name) private userModel: Model<User>
-  ) {}
+  ) { }
 
   async sendNotificationToAllUsers(title: string, body: string) {
     const payload = {
@@ -33,30 +33,31 @@ export class FcmNotificationService {
         age: "21"
       }
     };
-  
+
     try {
       const users = await this.userModel.find().select('notifications.fcm_token');
       const tokens = users.flatMap(user => user.notifications.map(notification => notification.fcm_token));
-      
+
       const uniqueTokens = [...new Set(tokens)];
       console.log(uniqueTokens);
-  
-  
+
+      const response = await admin.messaging().sendToDevice(uniqueTokens, payload);
+
       const notifications = uniqueTokens.map(token => ({
         fcm_token: token,
         title: title,
         body: body,
-        created_by: 'system' 
+        created_by: 'system'
       }));
-  
+
       await this.notificationModel.insertMany(notifications);
-  
+
       return { success: true, response };
     } catch (error) {
       return { success: false, error: error.message };
     }
   }
-  
+
   async getNotificationsByUserToken(token: string) {
     try {
       const user = await this.userModel.findOne({ 'notifications.fcm_token': token }).populate('notifications');
