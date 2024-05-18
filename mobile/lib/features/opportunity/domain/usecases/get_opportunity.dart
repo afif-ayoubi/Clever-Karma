24a7edca17a040197ce007f33744bcb434825e42
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-18 19:20:55 +0300

    created get usecase

---
+++ mobile/lib/features/opportunity/domain/usecases/get_opportunity.dart
@@ -0,0 +1,8 @@
+import 'package:mobile/core/common_domain/usecases/base_get.dart';
+import 'package:mobile/features/opportunity/domain/entities/opportunity.dart';
+
+
+
+class GetOpportunityUseCase extends GetUseCase<Opportunity> {
+  GetOpportunityUseCase(super.repository);
+}
