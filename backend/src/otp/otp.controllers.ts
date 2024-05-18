49fd78f8bf5db16b460a76b8be86003c0f8c62e9
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-18 10:07:25 +0300

    added send controller

--- backend/src/otp/otp.controllers.ts
+++ backend/src/otp/otp.controllers.ts
@@ -1,24 +1,19 @@
-import { Controller, Get, Res } from "@nestjs/common";
-import { OtpService } from "./otp.service";
-import { Response } from "express";  // Import the correct type from express
+import { Controller, Post, Body, BadRequestException } from '@nestjs/common';
+import { OtpService } from './otp.service';
 
-@Controller('mail')
+@Controller('otp')
 export class OtpController {
     constructor(private readonly otpService: OtpService) {}
 
-    @Get()
-    async sendMail(@Res() response: Response) {
-        try {
-            const mail = await this.otpService.sendMail();  
-            return response.status(200).json({
-                message: 'success',
-                mail,
-            });
-        } catch (error) {
-            return response.status(500).json({ 
-                message: 'Failed to send email',
-                error: error.message,
-            });
+    @Post('send')
+    async sendOtp(@Body('email') email: string): Promise<{ message: string }> {
+        if (!email) {
+            throw new BadRequestException('Email is required');
         }
+
+        await this.otpService.sendMail(email);
+        return { message: 'OTP sent successfully' };
     }
+
+ 
 }
