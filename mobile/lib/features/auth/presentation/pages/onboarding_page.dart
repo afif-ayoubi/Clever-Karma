04e8b00dcb0aa4e555525e8f8de15197c897336c
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-18 17:10:13 +0300

    added bloc

--- mobile/lib/features/auth/presentation/pages/onboarding_page.dart
+++ mobile/lib/features/auth/presentation/pages/onboarding_page.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mobile/core/constants/assets_manager.dart';
 import 'package:mobile/features/auth/presentation/widgets/onboarding_page/onboarding_detail_widget.dart';
 import 'package:mobile/routes/class_routes.dart';
 
+import '../bloc/obBoarding/on_boarding_bloc.dart';
+import '../bloc/obBoarding/on_boarding_event.dart';
+import '../bloc/obBoarding/on_boarding_state.dart';
 
-class OnBoardingPage extends StatefulWidget {
+
+class OnBoardingPage extends StatelessWidget {
   const OnBoardingPage({super.key});
 
   @override
-  State<OnBoardingPage> createState() => _OnBoardingPageState();
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => OnboardingBloc(),
+      child: OnBoardingView(),
+    );
+  }
 }
 
-class _OnBoardingPageState extends State<OnBoardingPage> {
-  int currentIndex = 0;
-late PageController _pageController;
-  List<Map<String, dynamic>> onBoardingData = [
+class OnBoardingView extends StatelessWidget {
+  final PageController _pageController = PageController();
+
+  final List<Map<String, dynamic>> onBoardingData = [
     {
       'title': "Find Your Passion",
       'body': "What issues matter most to you",
@@ -37,44 +46,37 @@ late PageController _pageController;
     },
   ];
 
-  void _incrementIndex() {
-    if (currentIndex < onBoardingData.length - 1) {
-      setState(() {
-        currentIndex += 1;
-      });
-    } else {
-      context.push(Routes.authRoute);
-    }
-  }
-@override
-  void initState() {
-  _pageController = PageController();
-
-  super.initState();
-  }
-  @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: _buildBody(context),
-    );
-  }
-
-  Widget _buildBody(BuildContext context) {
-    return PageView.builder(
-      itemCount: onBoardingData.length,
-      controller: _pageController,
-      itemBuilder:(context,index)=> OnBoardingDetailWidget(
-        title: onBoardingData[currentIndex]['title'],
-        body: onBoardingData[currentIndex]['body'],
-        isFirst: onBoardingData[currentIndex]['isFirst'],
-        imgPath: onBoardingData[currentIndex]['imgPath'],
-        onPressed: _incrementIndex,
+      body: BlocConsumer<OnboardingBloc, OnboardingState>(
+        listener: (context, state) {
+          if (state.currentIndex >= onBoardingData.length) {
+            context.go(Routes.authRoute);
+          }
+        },
+        builder: (context, state) {
+          return PageView.builder(
+            itemCount: onBoardingData.length,
+            controller: _pageController,
+            onPageChanged: (index) {
+              context.read<OnboardingBloc>().add(IncrementIndex());
+            },
+            itemBuilder: (context, index) => OnBoardingDetailWidget(
+              title: onBoardingData[state.currentIndex]['title'],
+              body: onBoardingData[state.currentIndex]['body'],
+              isFirst: onBoardingData[state.currentIndex]['isFirst'],
+              imgPath: onBoardingData[state.currentIndex]['imgPath'],
+              onPressed: () {
+                if (state.currentIndex == onBoardingData.length - 1) {
+                  context.go(Routes.authRoute);
+                } else {
+                  context.read<OnboardingBloc>().add(IncrementIndex());
+                }              },
+            ),
+          );
+        },
       ),
     );
   }
