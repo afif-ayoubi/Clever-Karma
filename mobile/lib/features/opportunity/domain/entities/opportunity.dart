4bbcd69d4952a3b860cf961fc879725c5e6ac6e1
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-18 19:21:07 +0300

    added hive

--- mobile/lib/features/opportunity/domain/entities/opportunity.dart
+++ mobile/lib/features/opportunity/domain/entities/opportunity.dart
@@ -1,20 +1,35 @@
-import 'package:mobile/core/constants/assets_manager.dart';
+import 'package:equatable/equatable.dart';
+import 'package:hive/hive.dart';
 
-class Opportunity {
+import '../../../../core/constants/assets_manager.dart';
+
+@HiveType(typeId: 1)
+class Opportunity extends HiveObject with EquatableMixin {
+  @HiveField(0)
   final int id;
+  @HiveField(1)
   final String name;
+  @HiveField(2)
   final String imageUrl;
+  @HiveField(3)
   final String description;
 
-  const Opportunity(
-      {required this.id, required this.name, required this.imageUrl, required this.description});
+   Opportunity({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+    required this.description,
+  });
 
   static List<Opportunity> fakeData = [
-    const Opportunity(
+     Opportunity(
         id: 1, name: 'blood Donation', imageUrl: AppImages.imgImg1, description: ''),
-    const Opportunity(
+     Opportunity(
         id: 2, name: 'blood Section', imageUrl: AppImages.onBoardingImg3, description: ''),
-    const Opportunity(
+     Opportunity(
         id: 3, name: 'blood Room', imageUrl: AppImages.onBoardingImg1, description: ''),
   ];
+
+  @override
+  List<Object?> get props => [id, name, imageUrl, description];
 }
