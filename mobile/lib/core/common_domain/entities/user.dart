034ffbdb95dc503ee0ee051bbd500c2299eef641
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-18 12:49:12 +0300

    added some methods

--- mobile/lib/core/common_domain/entities/user.dart
+++ mobile/lib/core/common_domain/entities/user.dart
@@ -1,7 +1,6 @@
 import 'package:equatable/equatable.dart';
 
-
-class User  with EquatableMixin {
+class User with EquatableMixin {
   final int? id;
 
   final String? firstName;
@@ -9,26 +8,35 @@ class User  with EquatableMixin {
   final String email;
   final String password;
   final String role;
+  final String? fcmToken;
 
+  User(
+      {this.id,
+      this.firstName,
+      required this.role,
+      this.lastName,
+      required this.email,
+      required this.password,
+      this.fcmToken});
 
-  User({
-    this.id,
-    this.firstName,
-    required this.role,
-    this.lastName,
-    required this.email,
-    required this.password,
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      firstName: json['firstName'] ?? "",
+      lastName: json['lastName'] ?? '',
+      email: json['email'],
+      password: json['password'] ?? '',
+      role: json['role'],
+      fcmToken: json['fcmToken'] ?? '',
+    );
+  }
+  Map<String, dynamic> toJson() => {
+    'email': email,
+    'password':password,
+    'role':role,
 
-  });
-
+  };
   @override
-  List<Object?> get props => [
-    id,
-    firstName,
-    lastName,
-    email,
-    password,
-    role,
-
-  ];
+  List<Object?> get props =>
+      [id, firstName, lastName, email, password, role, fcmToken];
 }
