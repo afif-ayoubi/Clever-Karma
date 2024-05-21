9eb7bd1e7ebfa0f0e1a0031d6c3a540a1c76133b
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-21 08:46:08 +0300

    changed the list

--- website/constants/dashboard.tsx
+++ website/constants/dashboard.tsx
@@ -26,37 +26,23 @@ import {
           icon: <MdSupervisedUserCircle />,
         },
         {
-          title: "Products",
+          title: "Sections",
           path: "/dashboard/products",
-          icon: <MdShoppingBag />,
-        },
-        {
-          title: "Transactions",
-          path: "/dashboard/transactions",
-          icon: <MdAttachMoney />,
-        },
-      ],
-    },
-    {
-      title: "Analytics",
-      list: [
-        {
-          title: "Revenue",
-          path: "/dashboard/revenue",
           icon: <MdWork />,
         },
         {
-          title: "Reports",
-          path: "/dashboard/reports",
-          icon: <MdAnalytics />,
+          title: "Notification",
+          path: "/dashboard/transactions",
+          icon: <MdPeople />,
         },
         {
-          title: "Teams",
-          path: "/dashboard/teams",
-          icon: <MdPeople />,
+          title: "Ai prediction",
+          path: "/dashboard/transactions",
+          icon: <MdAnalytics />,
         },
       ],
     },
+ 
     {
       title: "User",
       list: [
